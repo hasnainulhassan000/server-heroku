575a3e6df32aff98c2e9624aba08ae521ad79371
hasnainulhassan000 <74594022+hasnainulhassan000@users.noreply.github.com>
2020-12-21 13:25:38 +0500

    Update app.js

--- app.js
+++ app.js
@@ -18,6 +18,6 @@ server.get("/food", (req, res, next) => {
     res.send("here is food");
 })
 
-server.listen(3000, () => {
+server.listen(PORT, () => {
     console.log("server is running on port: "  + PORT);
 })
